a5c8299242b62a56df2ba9a9be462dbcbb1a79be
Clay McClure <clay@daemons.net>
2017-03-29 10:24:19 -0700

    void: copy src files to dst root
    As a first pass, simply mirror the src root to the dst root,
    creating dst dirs as necessary.

---
+++ void/build.py
@@ -0,0 +1,77 @@
+import os
+import shutil
+
+
+def build(srcroot, dstroot):
+    """
+    Copy the contents of srcroot to dstroot, recursively,
+    rendering Markdown files to HTML along the way.
+    """
+    for src, dst in zip(os.fwalk(srcroot), os.fwalk(dstroot)):
+        srcdir, srcdirs, srcfiles, srcdirfd = src
+        dstdir, dstdirs, dstfiles, dstdirfd = dst
+
+
+        build_dirs(srcdirs, dstdirs, dstdir)
+        build_files(srcfiles, dstfiles, srcdir, dstdir)
+
+def build_dirs(srcdirs, dstdirs, dstdir):
+    srcdirset = set(srcdirs)
+    dstdirset = set(dstdirs)
+
+    # remove directories in dstdirs not in srcdirs
+    for d in (dstdirset - srcdirset):
+        print("rmtree {}".format(os.path.join(dstdir, d)))
+        shutil.rmtree(os.path.join(dstdir, d))
+        dstdirset.remove(d)
+
+    # create directories in srcdirs not in dstdirs
+    for d in (srcdirset - dstdirset):
+        print("mkdir {}".format(os.path.join(dstdir, d)))
+        os.mkdir(os.path.join(dstdir, d))
+        dstdirset.add(d)
+
+    # dstdirs must now equal srcdirs; make it so
+    dstdirs[:] = srcdirs
+
+def build_files(srcfiles, dstfiles, srcdir, dstdir):
+    dstfileset = set(dstfiles)
+
+    for f in srcfiles:
+        f = copy_or_render(f, srcdir, dstdir)
+        dstfileset.discard(f)
+
+    # remove files in dstdir not in srcdir
+    for f in dstfileset:
+        print("rm {}".format(os.path.join(dstdir, f)))
+        os.unlink(os.path.join(dstdir, f))
+
+def copy_or_render(srcfile, srcdir, dstdir):
+    base, ext = os.path.splitext(srcfile)
+    if ext in (".markdown", ".md"):
+        dstfile = "".join((base, os.extsep, "html"))
+        maybe(render,
+              os.path.join(srcdir, srcfile),
+              os.path.join(dstdir, dstfile))
+    else:
+        dstfile = srcfile
+        maybe(copy,
+              os.path.join(srcdir, srcfile),
+              os.path.join(dstdir, dstfile))
+    return dstfile
+
+def maybe(fn, src, dst):
+    if not os.path.exists(dst) or mtime(src) > mtime(dst):
+        fn(src, dst)
+
+def mtime(f):
+    return os.stat(f).st_mtime
+
+def copy(src, dst):
+    print("cp {} {}".format(src, dst))
+    shutil.copyfile(src, dst)
+
+def render(src, dst):
+    print("render {} {}".format(src, dst))
+    shutil.copyfile(src, dst)
+
--- void/void.py
+++ void/void.py
@@ -1,6 +1,8 @@
 import argparse
 import os
 
+from .build import build
+
 def main():
     parser = argparse.ArgumentParser(
         description="Render HTML content from Markdown source")
@@ -11,13 +13,16 @@ def main():
 
     args = parser.parse_args()
 
-    if not (os.path.exists(args.srcdir) and os.path.isdir(args.srcdir)):
+    if not (os.path.exists(args.srcdir) and
+            os.path.isdir(args.srcdir)):
         parser.error("{} is not a directory".format(args.srcdir))
 
     try:
         os.makedirs(args.dstdir, exist_ok=True)
     except OSError:
-        parser.error("{} is not a directory".format(args.dstdir))
+        parser.error("failed to create dstdir {}".format(args.dstdir))
+
+    build(args.srcdir, args.dstdir)
 
 if __name__ == "__main__":
     main()
